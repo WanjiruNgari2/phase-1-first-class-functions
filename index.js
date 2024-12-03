28c4e05db7b93c4871151ed584f8d1ee6186dc3d
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-04 02:43:40 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+//receivefunction
+function receivesAFunction (callback) {
+   //const spy = chai.spy;
+   //const expect
+    callback();
+}
+
+//anonymous function
+function returnsANamedFunction() {
+    return function famousSingers() {
+        return 'jhene aiko';
+    }
+}
+
+
+//returnsAnAnonymousFunction
+function returnsAnAnonymousFunction (){
+   return function (){
+return 'danceFool'
+   }
+
+}
+
